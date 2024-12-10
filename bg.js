97979350a81dc770ec02be5e7e11087d5635d66d
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-10 11:47:31 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -4,21 +4,21 @@ let f =async (a, b, c)=>
     typeof a == "object" ?
       (b = +a.menuItemId, a = a.selectionText, "create") :
       (b = [" - pedigreequery"," - jbis"," - sporthorse"," - allpedigree"].indexOf(
-        a.slice(c = a.lastIndexOf("-") - 1))) >= 0 && (a = a.slice(0, c)), "update"
-    ]({
-      url: b >= 0 ? (
-        a = a.trim().toLowerCase().replaceAll(" ", "+"),
-        !b || b > 2 ?
-          (b = b ? "https://www.allbreedpedigree.com/" : "https://www.pedigreequery.com/") +
-          ((await fetch (b + a + "2", {method: "HEAD"})).status < 201 ? "index.php?query_type=check&search_bar=horse&h=" + a + "&g=5&inbred=Standard" : a) :
-        (b < 2 ? "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" : "https://sporthorse-data.com/search/pedigree?keys=") + a
-      ) : (()=> {
-        c = ""
-        let  i = (a = a.trim()).length
-        while (c = ((b = a[--i]) < "{" ? b : b > "=" && b < "ヷ" ? "%a5%" + (b.charCodeAt() - 12288).toString(16) : b == "" ? "+" : b == "ー" ? "%a1%bc" : b == "の" ? "%a4%ce" : b == "Ⅱ" ? "II": "")  + c, i);
-        return "https://db.netkeiba.com/?pid=horse_list&word=" + c
-      })()
-    })
+        a.slice(c = a.lastIndexOf("-") - 1)) >= 0 && (a = a.slice(0, c)), "update")
+  ]({
+    url: b >= 0 ? (
+      a = a.trim().toLowerCase().replaceAll(" ", "+"),
+      !b || b > 2 ?
+        (b = b ? "https://www.allbreedpedigree.com/" : "https://www.pedigreequery.com/") +
+        ((await fetch (b + a + "2", {method: "HEAD"})).status < 201 ? "index.php?query_type=check&search_bar=horse&h=" + a + "&g=5&inbred=Standard" : a) :
+      (b < 2 ? "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" : "https://sporthorse-data.com/search/pedigree?keys=") + a
+    ) : (()=> {
+      c = ""
+      let  i = (a = a.trim()).length
+      while (c = ((b = a[--i]) < "{" ? b : b > "=" && b < "ヷ" ? "%a5%" + (b.charCodeAt() - 12288).toString(16) : b == "" ? "+" : b == "ー" ? "%a1%bc" : b == "の" ? "%a4%ce" : b == "Ⅱ" ? "II": "")  + c, i);
+      return "https://db.netkeiba.com/?pid=horse_list&word=" + c
+    })()
+  })
 chrome.contextMenus.onClicked.addListener(f)
 chrome.omnibox.onInputEntered.addListener(f)
 }
