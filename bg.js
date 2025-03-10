9707643dd65f8cd80810542332fd2ee8f855d328
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-10 09:20:34 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,44 +1,39 @@
 {
   let open = async (_q, id, index) => {
     let q = _q.trim();
-    let props = {
-      url: id != 1
-        ? (
-          q = q.replaceAll(" ", "+"),
-          id == 2
-          ? "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" + q
-            : (q = q.normalize("NFD").replace(/[^a-zA-Z+-]/g, ""), id == 3)
-              ? "https://sporthorse-data.com/search/pedigree?keys=" + q
-              : (id = id ? "https://www.allbreedpedigree.com/" : "https://www.pedigreequery.com/" ) +
-              ((await fetch(id + q + "2", { method: "HEAD" })).status == 200
-                  ? "index.php?query_type=check&search_bar=horse&h=" + q + "&g=5&inbred=Standard"
-                  : q.toLowerCase())
-        )
-        : (()=> {
-            let url = "https://db.netkeiba.com/?pid=horse_list&word=";
-            let i = 0;
-            while (i < q.length) {
-              let charCode = q.charCodeAt(i);
-              url +=
-                charCode == 32
-                ? "+"
-                : charCode < 123
-                ? q[i]
-                : charCode > 12448 && charCode < 12535
-                ? "%a5%" + (charCode - 12288).toString(16)
-                : charCode > 12352 && charCode < 12436
-                ? "%a4%" + (charCode - 12192).toString(16)
-                : charCode == 12540
-                ? "%a1%bc"
-                : charCode == 8545
-                ? "II"
-                : "";
-              ++i;
-            }
-            return url;
-          })()
-    };
-    chrome.tabs[index ? (props.index = index, "create") : "update"](props);
+    let url = id != 1
+      ? (q = q.replaceAll(" ", "+"), id == 2)
+        ? "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" + q
+        : (q = q.normalize("NFD").replace(/[^a-zA-Z+-]/g, ""), id == 3)
+          ? "https://sporthorse-data.com/search/pedigree?keys=" + q
+          : (id = id ? "https://www.allbreedpedigree.com/" : "https://www.pedigreequery.com/" ) +
+            ((await fetch(id + q + "2", { method: "HEAD" })).status == 200
+              ? "index.php?query_type=check&search_bar=horse&h=" + q + "&g=5&inbred=Standard"
+              : q.toLowerCase())
+      : (()=> {
+          let url = "https://db.netkeiba.com/?pid=horse_list&word=";
+          let i = 0;
+          while (i < q.length) {
+            let charCode = q.charCodeAt(i);
+            url +=
+              charCode == 32
+              ? "+"
+              : charCode < 123
+              ? q[i]
+              : charCode > 12448 && charCode < 12535
+              ? "%a5%" + (charCode - 12288).toString(16)
+              : charCode > 12352 && charCode < 12436
+              ? "%a4%" + (charCode - 12192).toString(16)
+              : charCode == 12540
+              ? "%a1%bc"
+              : charCode == 8545
+              ? "II"
+              : "";
+            ++i;
+          }
+          return url;
+      })();
+    index ? chrome.tabs.create({ url, index }) : chrome.tabs.update({ url });
   }
   let searchFromContextMenus = async (info, tab) => navigator.onLine && open(
     info.selectionText,
