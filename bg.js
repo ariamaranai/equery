050a04d594ad65e320005a00a560c862897eb9fa
a <106607480+ariamaranai@users.noreply.github.com>
2025-09-13 06:39:48 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,71 +1,53 @@
 {
   let f = (_q, id, index) => {
     let q = _q.trim();
-    let url = id != 1
-      ? (q = q.replaceAll(" ", "+"), id == 2)
-        ? "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" + q
-        : (q = q.normalize("NFD").replace(/[^a-zA-Z+-]/g, ""), id == 3)
-          ? "https://sporthorse-data.com/search/pedigree?keys=" + q
-          : id != 5
-            ?
-              (id
-                ? "https://www.allbreedpedigree.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h="
-                : "https://www.pedigreequery.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h=") + q
-            : "https://www.horsetelex.com/horses/search?name=" + q
-      : (()=> {
-          let url = "https://db.netkeiba.com/?pid=horse_list&word=";
-          let i = 0;
-          while (i < q.length) {
-            let charCode = q.charCodeAt(i);
-            url +=
-              charCode == 32
-              ? "+"
-              : charCode < 123
-              ? q[i]
-              : charCode > 12448 && charCode < 12535
-              ? "%a5%" + (charCode - 12288).toString(16)
-              : charCode > 12352 && charCode < 12436
-              ? "%a4%" + (charCode - 12192).toString(16)
-              : charCode == 12540
-              ? "%a1%bc"
-              : charCode == 8545
-              ? "II"
-              : "";
-            ++i;
-          }
-          return url;
-      })();
+    let url = 0;
+    if (id != 1) {
+      if (id == 2)
+        q = q.replaceAll(" ", "+"),
+        url = "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" + q
+      else if (id == 3)
+        q = q.normalize("NFD").replace(/[^a-zA-Z+-]/g, ""),
+        url = "https://sporthorse-data.com/search/pedigree?keys=" + q
+      else
+        url = (
+          id != 5 ? id ? "https://www.allbreedpedigree.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h="
+                       : "https://www.pedigreequery.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h="
+          : "https://www.horsetelex.com/horses/search?name="
+        ) + q;
+    } else {
+      url = "https://db.netkeiba.com/?pid=horse_list&word=";
+      let i = 0;
+      while (i < q.length) {
+        let charCode = q.charCodeAt(i);
+        url +=
+            charCode == 32 ? "+"
+          : charCode < 123 ? q[i]
+          : charCode > 12448 && charCode < 12535 ? "%a5%" + (charCode - 12288).toString(16)
+          : charCode > 12352 && charCode < 12436 ? "%a4%" + (charCode - 12192).toString(16)
+          : charCode == 12540 ? "%a1%bc"
+          : charCode == 8545 ? "II" : "";
+        ++i;
+      }
+    }
     index
       ? chrome.tabs.create({ url, index })
       : chrome.tabs.update({ url });
   }
   chrome.contextMenus.onClicked.addListener(async (info, tab) =>
-    navigator.onLine && f(
-      info.selectionText,
-      +info.menuItemId,
-      tab.index + 1 || (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0].id + 1
-    )
+    f(info.selectionText, +info.menuItemId, tab.index + 1 || (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0].id + 1)
   );
   chrome.omnibox.onInputEntered.addListener(q => {
-    if (navigator.onLine) {
-      let id = 0;
-      f(
-        q.slice(0,
-            q.slice(-11) == " - netkeiba"
-          ? (id = 1, -11)
-          : q.slice(-7) == " - jbis"
-          ? (id = 2, -7)
-          : q.slice(-13) == " - sporthorse"
-          ? (id = 3, -13)
-          : q.slice(-14) == " - allpedigree"
-          ? (id = 4, -14)
-          : q.slice(-13) == " - horsetelex"
-          ? (id = 5, -13)
-          : q.length
-        ),
-        id
-      );
-    }
+    let id = 0;
+    f(q.slice(0,
+      q.slice(-11) == " - netkeiba" ? (id = 1, -11) :
+      q.slice(-7) == " - jbis" ? (id = 2, -7) :
+      q.slice(-13) == " - sporthorse" ? (id = 3, -13) :
+      q.slice(-14) == " - allpedigree" ? (id = 4, -14) :
+      q.slice(-13) == " - horsetelex" ? (id = 5, -13) :
+      q.length),
+      id
+    );
   });
 }
 chrome.omnibox.onInputChanged.addListener((q, suggest) => {
@@ -89,6 +71,7 @@ chrome.omnibox.onInputChanged.addListener((q, suggest) => {
 });
 chrome.runtime.onInstalled.addListener(() => {
   let i = 0;
+  let id = "0";
   while (
     chrome.contextMenus.create({
       title: [
@@ -99,9 +82,9 @@ chrome.runtime.onInstalled.addListener(() => {
         "%s - allpedigree",
         "%s - horsetelex"
       ][i],
-      id: i + "",
+      id,
       contexts: ["selection"]
     }),
     i < 5
-  ) ++i;
+  ) id = ++i + "";
 });
