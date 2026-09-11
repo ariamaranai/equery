7d8729e205be17943e22f4236022359f3fb666a7
a <106607480+ariamaranai@users.noreply.github.com>
2026-09-11 10:04:45 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,44 +1,5 @@
-{
-  let f = (_q, id, index, url) => {
-    let q = _q.trim();
-    if (id == 1) {
-      url = "https://db.netkeiba.com/horse/list.html?word=";
-      let i = 0;
-      while (i < q.length) {
-        let cc = q.charCodeAt(i);
-        url +=
-            cc == 32 ? "+"
-          : cc < 123 ? String.fromCharCode(cc)
-          : cc > 12448 && cc < 12535 ? "%a5%" + (cc - 12288).toString(16)
-          : cc > 12352 && cc < 12436 ? "%a4%" + (cc - 12192).toString(16)
-          : cc == 12540 ? "%a1%bc"
-          : cc == 8545 ? "II" : "";
-        ++i;
-      }
-    } else {
-      q = q.replaceAll(" ", "+");
-      if (id == 2)
-        url = "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" + q;
-      else if (id == 3)
-        url = "https://www.studbook.jp/users/ja/SearchBameiList?initial_forward=" + q.replace(/（.*/, "");
-      else
-        url = (
-          id == 4 ? "https://sporthorse-data.com/search/pedigree?keys=" :
-          id == 6 ? "https://www.horsetelex.com/horses/search?name=" :
-          id ? "https://www.allbreedpedigree.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h="
-             : "https://www.pedigreequery.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h="
-        ) + q.normalize("NFD").replace(/[^a-zA-Z+-]/g, "");
-    }
-    return index ? chrome.tabs.create({ url, index }) : chrome.tabs.update({ url });
-  }
-  chrome.contextMenus.onClicked.addListener(async (info, tab) =>
-    f(info.selectionText, +info.menuItemId, tab.index + 1 || (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0].id + 1)
-  );
-  chrome.omnibox.onInputEntered.addListener(q => {
-    let match = q.match(/ - (netkeiba|jbis|studbook|sporthorse|allpedigree|horsetelex)$/);
-    return f(match ? q.slice(0, match.index) : q, match && { netkeiba: 1, jbis: 2, studbook: 3, sporthorse: 4, allpedigree: 5, horsetelex: 6 }[match[1]]);
-  });
-}
+onunhandledrejection = e => e.preventDefault();
+
 chrome.omnibox.onInputChanged.addListener((q, suggest, $0) => {
   chrome.omnibox.setDefaultSuggestion({ description: q + " - pedigreequery" });
   let hosts = [" - netkeiba"," - jbis"," - studbook"," - sporthorse"," - allpedigree"," - horsetelex"];
@@ -61,3 +22,61 @@ chrome.runtime.onInstalled.addListener(() => {
   );
   return;
 });
+
+{
+  let f = (_q, id, index, url) => {
+    console.log(id);
+    let q = _q.trim();
+    if (id == 1) {
+      url = "https://db.netkeiba.com/horse/list.html?word=";
+      let i = 0;
+      while (i < q.length) {
+        let c = q[i];
+        url +=
+            c == " " ? "+"
+          : c < "{" ? c
+          : c == "ー" ? "%a1%bc"
+          : c == "Ⅱ" ? "II"
+          : (c = c.charCodeAt()) > 12448 && c < 12535 ? "%a5%" + (c - 12288).toString(16)
+          : c > 12352 && cc < 12436 ? "%a4%" + (cc - 12192).toString(16)
+          : "";
+        ++i;
+      }
+    } else {
+      q = q.replaceAll(" ", "+");
+      if (id == 2)
+        url = "https://www.jbis.or.jp/horse/result/?sid=horse&keyword=" + q;
+      else if (id == 3)
+        url = "https://www.studbook.jp/users/ja/SearchBameiList?initial_forward=" + q.replace(/（.*/, "");
+      else
+        url = (
+          id == 4 ? "https://sporthorse-data.com/search/pedigree?keys=" :
+          id == 6 ? "https://www.horsetelex.com/horses/search?name=" :
+          id == 0 ? "https://www.pedigreequery.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h=" :
+                    "https://www.allbreedpedigree.com/index.php?query_type=check&search_bar=horse&g=5&inbred=Standard&h="
+        ) + q.normalize("NFD").replace(/[^a-zA-Z+-]/g, "");
+    }
+    return index ? chrome.tabs.create({ url, index }) : chrome.tabs.update({ url });
+  }
+
+  chrome.contextMenus.onClicked.addListener(({ menuItemId, selectionText }, { index }) =>
+    ++index
+      ? chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => f(selectionText, menuItemId, tabs[0].id))
+      : f(selectionText, menuItemId, index)
+  );
+
+  chrome.omnibox.onInputEntered.addListener(q => {
+    let index = q.indexOf(" - ");
+    return index < 0
+      ? f(q, 0)
+      : f(
+          q.slice(0, index),
+          (q = q[index + 4]) == "e" ? 1 :
+                           q == "b" ? 2 :
+                           q == "t" ? 3 :
+                           q == "p" ? 4 :
+                           q == "l" ? 5 :
+                           q == "o" ? 6 : 0
+        );
+  });
+}
